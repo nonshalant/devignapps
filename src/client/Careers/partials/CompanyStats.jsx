616eff4246bd5951c874459b88c08f4d2eac7211
nonshalant <shamir.gibbons@gmail.com>
2026-03-03 03:02:48 -0500

    UPDATE: company details

--- src/client/Careers/partials/CompanyStats.jsx
+++ src/client/Careers/partials/CompanyStats.jsx
@@ -4,11 +4,11 @@ const CompanyStats = () => {
   return (
     <div className='stats'>
         <div className="container">
-            <h1>10+</h1>
+            <h1>3+</h1>
             <p>Team members across the USA</p>
         </div>
         <div className="container">
-            <h1>5+</h1>
+            <h1>20+</h1>
             <p>People and companies helped</p>
         </div>
     </div>
